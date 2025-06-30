6e90864d36049c3ead61812cd69f0cc29ef79b72
Akshat Bansal <akshatkba24@gmail.com>
2025-06-30 22:08:54 +0530

    added v1 for tweet and hashtag model

--- src/index.js
+++ src/index.js
@@ -5,6 +5,5 @@ const app = express();
 app.listen(3000, async () => {
     console.log('Server listening on port 3000');
     await connect();
-    console.log(('Mongo DB connected!!'));
-    
+    console.log(('Mongo DB connected!!')); 
 });
